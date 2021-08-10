c9d7b8bf7bf744a4cbfb4137f1113edae2708902
chen2020i <2021694476@qq.com>
2021-08-10 20:40:21 +0800

    Site updated: 2021-08-10 20:40:20

--- js/qipao.js
+++ js/qipao.js
@@ -1,1 +1,67 @@
-if(-1!==location.href.indexOf("link")){var imgObj=document.getElementsByTagName("img");for(i=0;i<imgObj.length;i++)imgObj[i].onerror=function(){this.src="https://cdn.jsdelivr.net/gh/tzy13755126023/BLOG_SOURCE/theme_f/friend_404.gif"}}$((function(){var e;(e=jQuery).fn.circleMagic=function(t){var o,n,a,i,r=!0,c=[],d=e.extend({color:"rgba(255,0,0,.5)",radius:10,density:.3,clearOffset:.2},t),l=this[0];function s(){if(r)for(var e in i.clearRect(0,0,o,n),c)c[e].draw();requestAnimationFrame(s)}function h(){var e=this;function t(){e.pos.x=Math.random()*o,e.pos.y=n+100*Math.random(),e.alpha=.1+Math.random()*d.clearOffset,e.scale=.1+.3*Math.random(),e.speed=Math.random(),"random"===d.color?e.color="rgba("+Math.floor(255*Math.random())+", "+Math.floor(0*Math.random())+", "+Math.floor(0*Math.random())+", "+Math.random().toPrecision(2)+")":e.color=d.color}e.pos={},t(),this.draw=function(){e.alpha<=0&&t(),e.pos.y-=e.speed,e.alpha-=5e-4,i.beginPath(),i.arc(e.pos.x,e.pos.y,e.scale*d.radius,0,2*Math.PI,!1),i.fillStyle=e.color,i.fill(),i.closePath()}}!function(){o=l.offsetWidth,n=l.offsetHeight,function(){var e=document.createElement("canvas");e.id="canvas",e.style.top=0,e.style.zIndex=0,e.style.position="absolute",l.appendChild(e),e.parentElement.style.overflow="hidden"}(),(a=document.getElementById("canvas")).width=o,a.height=n,i=a.getContext("2d");for(var e=0;e<o*d.density;e++){var t=new h;c.push(t)}s()}(),window.addEventListener("scroll",(function(){r=!(document.body.scrollTop>n)}),!1),window.addEventListener("resize",(function(){o=l.clientWidth,n=l.clientHeight,l.height=n+"px",a.width=o,a.height=n}),!1)},$("#page-header").circleMagic({radius:10,density:.2,color:"rgba(255,255,255,.4)",clearOffset:.99})}));
+/*
+ * @Author: tzy1997
+ * @Date: 2020-12-15 20:55:25
+ * @LastEditors: tzy1997
+ * @LastEditTime: 2021-01-12 19:02:25
+ */
+
+// 友情链接页面 头像找不到时 替换图片
+if (location.href.indexOf("link") !== -1) {
+    var imgObj = document.getElementsByTagName("img");
+    for (i = 0; i < imgObj.length; i++) {　　
+        imgObj[i].onerror = function() { this.src = "https://cdn.jsdelivr.net/gh/tzy13755126023/BLOG_SOURCE/theme_f/friend_404.gif" }
+    }
+}
+
+$(function() {
+
+    // 气泡
+    function bubble() {
+        $('#page-header').circleMagic({
+            radius: 10,
+            density: .2,
+            color: 'rgba(255,255,255,.4)',
+            clearOffset: 0.99
+        });
+    }! function(p) {
+        p.fn.circleMagic = function(t) {
+            var o, a, n, r, e = !0,
+                i = [],
+                d = p.extend({ color: "rgba(255,0,0,.5)", radius: 10, density: .3, clearOffset: .2 }, t),
+                l = this[0];
+
+            function c() { e = !(document.body.scrollTop > a) }
+
+            function s() { o = l.clientWidth, a = l.clientHeight, l.height = a + "px", n.width = o, n.height = a }
+
+            function h() {
+                if (e)
+                    for (var t in r.clearRect(0, 0, o, a), i) i[t].draw();
+                requestAnimationFrame(h)
+            }
+
+            function f() {
+                var t = this;
+
+                function e() { t.pos.x = Math.random() * o, t.pos.y = a + 100 * Math.random(), t.alpha = .1 + Math.random() * d.clearOffset, t.scale = .1 + .3 * Math.random(), t.speed = Math.random(), "random" === d.color ? t.color = "rgba(" + Math.floor(255 * Math.random()) + ", " + Math.floor(0 * Math.random()) + ", " + Math.floor(0 * Math.random()) + ", " + Math.random().toPrecision(2) + ")" : t.color = d.color }
+                t.pos = {}, e(), this.draw = function() { t.alpha <= 0 && e(), t.pos.y -= t.speed, t.alpha -= 5e-4, r.beginPath(), r.arc(t.pos.x, t.pos.y, t.scale * d.radius, 0, 2 * Math.PI, !1), r.fillStyle = t.color, r.fill(), r.closePath() }
+            }! function() {
+                o = l.offsetWidth, a = l.offsetHeight,
+                    function() {
+                        var t = document.createElement("canvas");
+                        t.id = "canvas", t.style.top = 0, t.style.zIndex = 0, t.style.position = "absolute", l.appendChild(t), t.parentElement.style.overflow = "hidden"
+                    }(), (n = document.getElementById("canvas")).width = o, n.height = a, r = n.getContext("2d");
+                for (var t = 0; t < o * d.density; t++) {
+                    var e = new f;
+                    i.push(e)
+                }
+                h()
+            }(), window.addEventListener("scroll", c, !1), window.addEventListener("resize", s, !1)
+        }
+    }(jQuery);
+
+    // 调用气泡方法
+    bubble();
+})
+
+
